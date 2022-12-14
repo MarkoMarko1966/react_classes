c47e3ffb7c048aa06bb4dd30cfee2a115b66e528
MarkoMarko1966 <markomickovski1966.mm@gmail.com>
2022-12-14 22:07:59 +0100

    cas1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Student from "./components/Student";
+
+const App = () => {
+
+  const name = "Mia Koluci"
+  const age = 34
+  const toShow = true
+  const fruits = [`apple`, `android`, `banana`]
+
+const student = {
+  name,
+  age,
+  fruits,
+}
+
+  return (<>
+    <div>Hello World</div>
+    <Student name={name} age={age} shouldTheComponentShow={toShow}/>
+  </>);
+}
+
+export default App;
